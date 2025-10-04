07d63d14b064756340545b29f820893a0616a727
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2025-10-04 15:13:16 -0300

    Add room

--- backend/logica/main.py
+++ backend/logica/main.py
@@ -29,39 +29,18 @@ import json
 
 
 with open("restricciones.json", "r") as f:
-    archivo = json.load(f)
+    restricciones = json.load(f)
 
+with open("rooms.json") as f:
+    rooms = json.load(f)
 
 if __name__ == "__main__":
 
-
-    rooms = [
-        "Group Social-2 (Table) / Meal Consumption / Mission Planning-1 (Table)",   
-      "Human Waste-1 (Waste Collection)",
-      "Human Waste-2 (Cleansing) / Hygiene-1 (Cleansing)",
-      "Waste Management",
-      "Meal Preparation-1 (Food Prep)",
-      "Private Habitation-2 (Sleep & Relaxation) / Hygiene-2 (Non-Cleansing)",
-      'Meal Preparation-2 (Work Surface)',
-      'Exercise-1 (Cycle Ergometer)',
-      'Exercise-2 (Treadmill)',
-      'Exercise-3 (Resistive Device)',
-      'Group Social-1 (Open Area) / Mission Planning-3 (Training)',
-      'Maintenance-2 (Work Surface) / Logistics-1 (Work Surface) / EVA-1 (Suit Testing)',
-       'Logistics-2 (Temporary Stowage)',
-        'Maintenance-1 (Computer) / EVA-2 (EVA Computer/Data)',
-        'Mission Planning-2 (Computer/Command) / Spacecraft Monitoring',
-        'Medical-1 (Computer)',
-        'Medical-3 (Medical Care)',
-        'Private Habitation-1 (Work Surface) / Medical-2 (Ambulatory Care)',
-        'Mantención'
-      ]
-
     # Prohibiciones (adyacencia 0)
-    zero_pairs = archivo["zero_pairs"]
+    zero_pairs = restricciones["zero_pairs"]
 
     # Preferencias (>1). Por defecto, lo no listado vale 1.
-    prefs = archivo["preferences"]
+    prefs = restricciones["preferences"]
 
     A, idx = matriz_adyacencia(rooms, zero_pairs, prefs, default_weight=1)
 
