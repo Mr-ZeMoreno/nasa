b358477215dd2230e3202634d7789096698de070
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2025-10-04 11:04:51 -0300

    Iniciar scripts

---
+++ backend/logica/nodo.py
@@ -0,0 +1,25 @@
+class Adyacencia:
+    def __init__(self) -> None:
+        self.nodos = [None] * 6
+        # self.index = 
+    
+    def add_nodo(self, nodo):
+        # self.nodos.
+        pass
+
+class Nodo:
+    def __init__(self):
+        self.peso = None
+        
+
+# Un nodo se puede unir con máximo 6 nodos
+# La adyacencia debe mostrarse como matriz
+# ejemplo : 
+# b -> a
+# c -> a
+# d -> a
+# e -> a
+# f -> a
+# g -> a
+# 
+# adyacencia = []
