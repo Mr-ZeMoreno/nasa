04824b9521319bbf7aa4a2898f3fc97f8bf734e4
Daniel Ignacio <di.avendanopinto@gmail.com>
2025-10-05 03:05:24 -0300

    front--

--- react/next.config.mjs
+++ react/next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 
-export default nextConfig
+  // se añadio esto 
+  async rewrites() {
+    return [
+      { source: '/landing',  destination: '/landing/index.html' },
+      { source: '/landing/', destination: '/landing/index.html' },
+    ];
+  },
+};
+
+export default nextConfig;
